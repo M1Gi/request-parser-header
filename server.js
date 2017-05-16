1dc25d68d830a045a1b10de9289be964b22985eb
Ryan Trang <rtrang16@gmail.com>
2017-05-16 07:43:46 +0000

    configure for deployment

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
-var express = require('express');
+'use strict'; // Define that JS code should be run in 'strict mode' 
+
+// Call the packages needed
+var express = require('express'); // Import Express module
+var app = express(); // Create an Express App
+var routes = require('./app/routes/index.js');
+
+var port = process.env.PORT || 8080; // Set our Port
+
+
+routes(app);
+
+app.listen(port, function() {
+    console.log('Node.js listening on port ' + port);
+});
+
