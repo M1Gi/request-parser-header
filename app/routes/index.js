093acc429dafe16a76ecf6038b8cd7ffd373b408
Ryan Trang <rtrang16@gmail.com>
2017-05-22 15:19:09 +0000

    configure for deployment to Heroku

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,17 +1,43 @@
-'use strict'; // Define that JS code should be run in 'strict mode'
+var express = require('express');
+var router = express.Router();
 
-module.exports = function(app) {
-    app.route('/')
-        .get(function(req, res) {
-            var ip = req.headers['x-forwarded for'] ||
-                    req.connection.remoteAddress ||
-                    req.socket.remoteAddress ||
-                    req.connection.socket.remoteAddress;
-        var info = {
-            'ip-address': ip,
-            'language': req.headers["accept-language"].split(',')[0],
-            'software': req.headers['user-agent'].split(') ')[0].split(' (')[1]
-        };
-        res.send(info);
-        });
-};
+router.get('/', function(req, res, next) {
+    /* Look for the ip address in 4 ways & when found, store in 'ip' var: 
+    1) By default, look in the 'headers' property of the HTTP Request. The headers are stored in a 
+    JS Object, w/ the header strings as object keys. Hence look for the 'x-forwarded-for' key to 
+    access the XFF de-facto standard header for identifying the originating IP address of the client 
+    connecting to a web server through an HTTP proxy or load balancer. */
+    var ip = req.headers['x-forwarded-for'] ||
+            /* 2) Look in the request object's 'connection' prop (which is a net.Socket object). The net.Socket 
+            object has a prop 'remoteAddress' which is a string representation of the remote IP address; access 
+            the IP address string in 'remoteAddress'. */
+            req.connection.remoteAddress ||
+            /* 3) Look in the request object's 'socket' prop for the string representation of the remote IP 
+            address. */
+            req.socket.remoteAddress ||
+            /* 4) Look in the req object's connection prop for a socket prop which is an obj with the 
+            'remoteAddress' prop (a string representation of the remote IP address); access the value of 
+            'remoteAddress'. */
+            req.connection.socket.remoteAddress;
+    // Create an object (info) that stores the IP address, language, & OS for the browser making the request 
+    var info = {
+        'ip-address': ip, // Set the IP Address key's value to the 'ip' variable
+        /* In the headers prop of the HTTP Request, access the 'accept-language' key's value (a string) & split 
+        the string into an Array at each comma; then access the first element in the array (index 0) which 
+        contains the language for the browser making the request; set this as the language key's value. */
+        'language': req.headers["accept-language"].split(',')[0],
+        /* In the headers prop of the HTTP Request, access the 'user-agen' key's value (a string) & split 
+        the string into an Array at 1) an inward parenthesis followed by a space ') ' then access that element 
+        (the 1st element in the array); 2) then split that 1st element into another array at an a space 
+        followed by an outward parenthesis ' (' then access the string after the split (the 2nd element in the 
+        array); set this as the software key's value (giving the OS Company Name and Operating System Version). */
+        'software': req.headers['user-agent'].split(') ')[0].split(' (')[1]
+    };
+    // Send back the info obj as HTTP response
+    res.send(info);
+});
+
+module.exports = router;
+
+
+
--- server.js
+++ server.js
@@ -1,16 +1,20 @@
-'use strict'; // Define that JS code should be run in 'strict mode' 
-
 // Call the packages needed
 var express = require('express'); // Import Express module
 var app = express(); // Create an Express App
+/* Set the Entry Point and store it in the var 'routes'. 
+-> The Entry Point is the JS file (index.js) that will be invoked when consumers of your 
+module 'require()' it (this file includes the main logic for your module). */
 var routes = require('./app/routes/index.js');
 
-var port = process.env.PORT || 8080; // Set our Port
+app.use('/', routes);
 
-
-routes(app);
-
+/* Access the PORT property in the object containing the user environment by default OR 
+use 8080 as the port if default not available. Store result in 'port' variable. */
+var port = process.env.PORT || 8080; 
+// Listen for connections on the specfied port and run a callback
 app.listen(port, function() {
+    // Print message describing what port Node.js is listening on
     console.log('Node.js listening on port ' + port);
 });
 
+module.exports = app;
